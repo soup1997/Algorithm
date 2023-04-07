fb9e0584a021b61901afe568a8fba91a28c161aa
Hyunseup Jo <hyunseup0815@gmail.com>
2023-04-07 12:13:19 +0900

    2023-04-07

--- Softeer/근무시간/근무시간.cpp
+++ Softeer/근무시간/근무시간.cpp
@@ -1,26 +1,43 @@
-#include <iostream>
+#include<iostream>
+#include<string>
 
 using namespace std;
 
-bool comparator(int A, int B)
-{
-    if (A > B)
-    {
-        cout << "A";
-    }
+int calc_time(string& start, string& end) {
+	int StartHour = stoi(start.substr(0, 2));
+	int StartMin = stoi(start.substr(3, 2));
 
-    else if (A < B)
-    {
-        cout << "B";
-    }
+	int EndHour = stoi(end.substr(0, 2));
+	int EndMin = stoi(end.substr(3, 2));
 
-    else
-        cout << "same";
+	int TotalHour = EndHour - StartHour;
+	int TotalMin = 0;
+	
+	if (StartMin < EndMin) {
+		TotalMin = EndMin - StartMin;
+	}
+
+	else if(StartMin > EndMin) {
+		TotalMin = (60 - StartMin) + EndMin;
+		TotalHour--;
+	}
+
+	else;
+
+	return (TotalHour) * 60 + TotalMin;
 }
-int main(int argc, char **argv)
+
+int main(int argc, char** argv)
 {
-    int A, B;
-    cin >> A >> B;
-    comparator(A, B);
-    return 0;
+	string start, end;
+	int res = 0;
+
+	for(auto i = 0; i < 5; i++) {
+		cin >> start >> end;
+		res += calc_time(start, end);
+	}
+
+	cout << res;
+
+	return 0;
 }
--- Softeer/주행거리 비교하기/주행거리 비교하기.cpp
+++ Softeer/주행거리 비교하기/주행거리 비교하기.cpp
@@ -1,43 +1,26 @@
-#include<iostream>
-#include<string>
+#include <iostream>
 
 using namespace std;
 
-int calc_time(string& start, string& end) {
-	int StartHour = stoi(start.substr(0, 2));
-	int StartMin = stoi(start.substr(3, 2));
-
-	int EndHour = stoi(end.substr(0, 2));
-	int EndMin = stoi(end.substr(3, 2));
-
-	int TotalHour = EndHour - StartHour;
-	int TotalMin = 0;
-	
-	if (StartMin < EndMin) {
-		TotalMin = EndMin - StartMin;
-	}
-
-	else if(StartMin > EndMin) {
-		TotalMin = (60 - StartMin) + EndMin;
-		TotalHour--;
-	}
-
-	else;
-
-	return (TotalHour) * 60 + TotalMin;
-}
-
-int main(int argc, char** argv)
+bool comparator(int A, int B)
 {
-	string start, end;
-	int res = 0;
+    if (A > B)
+    {
+        cout << "A";
+    }
 
-	for(auto i = 0; i < 5; i++) {
-		cin >> start >> end;
-		res += calc_time(start, end);
-	}
+    else if (A < B)
+    {
+        cout << "B";
+    }
 
-	cout << res;
-
-	return 0;
+    else
+        cout << "same";
+}
+int main(int argc, char **argv)
+{
+    int A, B;
+    cin >> A >> B;
+    comparator(A, B);
+    return 0;
 }
