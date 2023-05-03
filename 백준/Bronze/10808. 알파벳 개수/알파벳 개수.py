fb24fcc3929550f99ecb95e6be71d801b7d54676
Hyunseup Jo <soup1997@naver.com>
2023-05-03 13:39:05 +0900

    [Bronze IV] Title: 알파벳 개수, Time: 40 ms, Memory: 31388 KB -BaekjoonHub

---
+++ 백준/Bronze/10808. 알파벳 개수/알파벳 개수.py
@@ -0,0 +1,6 @@
+string = input()
+alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
+
+for i in alphabet:
+    cnt = string.count(i)
+    print(cnt, end=' ')
