8cec873e400ae5f9613776034aa4b731e5a2bf63
Hyunseup Jo <soup1997@naver.com>
2024-07-04 14:31:51 +0900

    [level 2] Title: 더 맵게, Time: 1623.45 ms, Memory: 51.7 MB -BaekjoonHub

--- 프로그래머스/2/42626. 더 맵게/더 맵게.py
+++ 프로그래머스/2/42626. 더 맵게/더 맵게.py
@@ -3,16 +3,17 @@ from heapq import *
 # python의 heapq는 default가 최소 힙
 
 def solution(scoville, K):
-    answer = 0
-    heapify(scoville)
-
-    while scoville[0] < K:
-        root1 = heappop(scoville)
-        root2 = heappop(scoville)
-        heappush(scoville, root1 + (2 * root2))
-        answer += 1
+    if K == 0:
+        return -1
+    
+    else:
+        answer = 0
+        heapify(scoville)
         
-        if len(scoville) ==1 and scoville[0] < K:
-            return -1
+        while scoville[0] < K:
+            root1 = heappop(scoville)
+            root2 = heappop(scoville)
+            heappush(scoville, root1 + (2 * root2))
+            answer += 1
 
-    return answer
+        return answer
