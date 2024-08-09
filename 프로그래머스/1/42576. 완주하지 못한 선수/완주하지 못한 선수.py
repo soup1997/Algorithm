4aba9b0303675e52d46fcf35bc1c9ffd7fb01baf
Hyunseup Jo <soup1997@naver.com>
2024-08-09 13:26:00 +0900

    [level 1] Title: 완주하지 못한 선수, Time: 74.55 ms, Memory: 26.3 MB -BaekjoonHub

--- 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
+++ 프로그래머스/1/42576. 완주하지 못한 선수/완주하지 못한 선수.py
@@ -1,8 +1,9 @@
 def solution(participant, completion):
     participant.sort()
-    completion.append("z")
     completion.sort()
-
-    for (i, j) in zip(participant, completion):
+    
+    for i, j in zip(participant, completion):
         if i != j:
             return i
+        
+    return participant[-1]
