6ad35ca4f5e7715eb734e53c8d015f54f2b2819c
Hyunseup Jo <soup1997@naver.com>
2024-08-12 14:02:55 +0900

    [level 1] Title: 체육복, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/1/42862. 체육복/체육복.py
+++ 프로그래머스/1/42862. 체육복/체육복.py
@@ -1,3 +1,26 @@
 def solution(n, lost, reserve):
-    answer = 0
+    lost.sort()
+    reserve.sort()
+    
+    for i in lost[:]: # 공통 요소 제거
+        if i in reserve:
+            reserve.remove(i)
+            lost.remove(i)
+            
+    lost_copied = lost[:] # deep copy
+    
+    for i in lost_copied:
+        if not(len(reserve)):
+            break
+        
+        else:
+            if i-1 in reserve:
+                reserve.remove(i-1)
+                lost.remove(i)
+
+            elif i+1 in reserve:
+                reserve.remove(i+1)
+                lost.remove(i)        
+
+    answer = n - len(lost)
     return answer
