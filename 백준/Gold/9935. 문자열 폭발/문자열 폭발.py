efd3c6c1cc037b198ae797588e6802cca892f7af
Hyunseup Jo <soup1997@naver.com>
2024-09-06 00:54:36 +0900

    [Gold IV] Title: 문자열 폭발, Time: 508 ms, Memory: 71552 KB -BaekjoonHub

--- 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
+++ 백준/Gold/9935. 문자열 폭발/문자열 폭발.py
@@ -1,28 +1,23 @@
 import sys
 
 string = list(sys.stdin.readline().strip())
-bomb = sys.stdin.readline().strip()
+bomb = list(sys.stdin.readline().strip())
+e = len(bomb)
 
 stack = []
 
 for char in string:
     stack.append(char)
     
-    if len(stack) >= len(bomb):
-        stringSeg = ''.join(stack[-len(bomb):])
+    if len(stack) >= e and stack[-1] == bomb[-1]:
+        stringSeg = ''.join(stack[-e:])
+        stringBomb = ''.join(bomb)
         
-        if stringSeg == bomb:
-            for _ in range(len(bomb)):
+        if stringSeg == stringBomb:
+            for _ in range(e):
                 stack.pop()
                        
 if stack:
     print(*stack, sep='')
 else:
-    print("FRULA")
-                     
-        
-    
-        
-        
-        
-    
+    print("FRULA")    
