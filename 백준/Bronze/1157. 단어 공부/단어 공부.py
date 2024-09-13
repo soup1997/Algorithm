e92e92f186bec72f6491eaf64b38663c70843092
Hyunseup Jo <soup1997@naver.com>
2024-09-13 21:26:47 +0900

    [Bronze I] Title: 단어 공부, Time: 68 ms, Memory: 33076 KB -BaekjoonHub

--- 백준/Bronze/1157. 단어 공부/단어 공부.py
+++ 백준/Bronze/1157. 단어 공부/단어 공부.py
@@ -1,16 +1,14 @@
 import sys
 
 string = sys.stdin.readline().strip().upper()
-string_hash = {c: string.count(c) for c in set(string)}
+string_set = list(set(string))
+char_cnt = [string.count(c) for c in string_set]
 
-values = list(string_hash.values())
-max_value = max(values)
+maximum = max(char_cnt)
 
-if values.count(max_value) != 1:
+if char_cnt.count(maximum) != 1: # maximum이 여러개 존재
     print("?")
 
-else:
-    for key in string_hash.keys():
-        if string_hash[key] == max_value:
-            print(key)
-            break
+else: # maximum이 1개만 존재
+    max_idx = char_cnt.index(maximum)
+    print(string_set[max_idx])
