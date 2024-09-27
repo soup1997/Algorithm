7accd5e68c07638818605ba9b5bfdecb81078516
Hyunseup Jo <soup1997@naver.com>
2024-09-27 10:36:03 +0900

    [Silver III] Title: 단어 뒤집기 2, Time: 228 ms, Memory: 31900 KB -BaekjoonHub

---
+++ 백준/Silver/17413. 단어 뒤집기 2/단어 뒤집기 2.py
@@ -0,0 +1,36 @@
+s = input().rstrip()
+idx = 0
+stack = []
+ans = []
+while idx < len(s):
+    stack.append(s[idx])
+    
+    if s[idx] == "<": # tag 검출
+        stack.pop() # append 취소
+        S = ''
+        while stack:
+            S += stack.pop()
+        if S != "":
+            ans.append(S)
+        start = idx
+        while s[idx] != ">": # tag 닫힐 때까지 idx 증가
+            idx += 1
+        ans.append(s[start:idx+1]) # tag 통째로 ans에 저장
+            
+    elif s[idx] == " ": # 공백 검출출
+        stack.pop() # append 취소
+        S = ''
+        while stack:
+            S += stack.pop()
+        ans.append(S)
+        ans.append(" ")
+    
+    elif idx == len(s)-1: # 문자열 끝 도달
+        S = ''
+        while stack:
+            S += stack.pop()
+        ans.append(S)
+    
+    idx += 1
+    
+print(*ans, sep="")
